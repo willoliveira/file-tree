ca04f7e4c98010c1df2ed3b68148071547417480
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-03-13 16:36:25 -0300

    Inicio do plugin

--- js/file-tree.js
+++ js/file-tree.js
@@ -1,5 +1,41 @@
-(function(){
+(function($){
 
+  var defaultOptions = {
+    templates: {
+      containerParent: "<ul class=\"container-parent\">",
+      containerChild: "<ul class=\"container-childs\">",
+      child: "<li><input class=\"checkButton\" type=\"checkbox\"/></li>"
+    }
+  },
+    FileTreeOptions;
 
+  //
+  $.fn.fileTree = FileTree;
 
-})()
+  function FileTree(options) {
+    console.log(options);
+    //se estiver dando um start no fileTree
+    if (typeof options == "object" || typeof options == "undefined") {
+      //configura o plugin
+      //da um merge com as prop default
+      FileTreeOptions = $.extend({}, defaultOptions, options);
+      //inicia o plugin
+      init();
+    }
+    return this;
+  }
+
+  function init() {
+    //Limpa a div e adiciona o container inicial
+    document.getElementById("fileTree").innerHTML = FileTreeOptions.templates.containerParent;
+    //cria os nós
+    FileTreeOptions.data.forEach(function(element, index) {
+      addNode(element);
+    });
+  }
+
+  function addNode(node) {
+
+  }
+
+})(jQuery)
