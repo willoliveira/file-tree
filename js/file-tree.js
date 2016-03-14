653581acccdae22920a3ec2c9142a03934d356e3
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-03-13 21:54:38 -0300

    Funcionalidade de check dos checkbox para os filhos

--- js/file-tree.js
+++ js/file-tree.js
@@ -1,13 +1,20 @@
 (function($){
-
+  /**
+  TODO:
+  - fazer uma referencia do checkbox na criação do nó e guardar uma referencia
+  */
+  "use strict";
   var defaultOptions = {
     templates: {
       containerParent: "<ul class=\"container-parent\">",
       containerChild: "<ul class=\"container-childs\">",
       child: "<li class=\"line\"><input class=\"checkButton\" type=\"checkbox\"/><ul class=\"container-childs\"></li>"
-    }
+    },
+    data: {},
+    arrSelected: []
   },
-    FileTreeOptions;
+    FileTreeOptions,
+    containerParent;
 
   //
   $.fn.fileTree = FileTree;
@@ -17,40 +24,105 @@
     containerParent = this;
     //se estiver dando um start no fileTree
     if (typeof options == "object" || typeof options == "undefined") {
+      let initialData = options.initialData;
+      delete options.initialData;
       //configura o plugin
       //da um merge com as prop default
       FileTreeOptions = $.extend({}, defaultOptions, options);
       //inicia o plugin
-      init();
+      init(initialData);
     }
     return this;
   }
 
-  function init() {
+  function init(initialData) {
     //Limpa a div e adiciona o container inicial
     document.getElementById("fileTree").innerHTML = FileTreeOptions.templates.containerParent;
+    //formata os dados
+    formatNodes(initialData);
     //cria os nós
-    FileTreeOptions.data.forEach(function(element, index) {
-      addNode(element);
+    for (var index in FileTreeOptions.data) {
+      addNode(FileTreeOptions.data[index]);
+    };
+  }
+
+  function formatNodes(arrData) {
+    arrData.forEach(function(element, index) {
+      //adiciona no objeto
+      FileTreeOptions.data["node-" + element.id] = element;
+      //adiciona um array para adicionar referencia para os filhos
+      FileTreeOptions.data["node-" + element.id].childs = [];
+      FileTreeOptions.data["node-" + element.id].check = false;
     });
   }
 
   function addNode(node) {
+    let container, $line, $checkbox;
+    //se não houver parent
     if (!node.idParent) {
-      var container = ".container-parent";
-      cont = $(containerParent).children(".container-parent");
-    } else
-      cont = $("#" + node.idParent + ".line > .container-childs");
+      container = $(containerParent).children(".container-parent");
+    } else {
+      container = $("#node-" + node.idParent + ".line > .container-childs");
+      //se tem um pai, adiciona voce como filho nele
+      FileTreeOptions.data["node-" + node.idParent].childs.push(node.id);
+    }
     //adiciona uma li
-    var $line = $(FileTreeOptions.templates.child).attr({id: node.id});
+    $line = $(FileTreeOptions.templates.child).attr({id: "node-" + node.id});
     //add click no check
-    $line.find(".checkButton").bind("mousedown", clickCheck.bind(this, node));
+    $checkbox = $line.find(".checkButton")
+    $line.find(".checkButton").bind("click", clickCheck.bind($checkbox, node, undefined));
     //adiciona o filho
-    $(cont).append($line);
+    $(container).append($line);
   }
 
-  function clickCheck(node) {
-    console.log("clickCheck", node);
+  function clickCheck(node, forceCheck) {
+    var isForced = typeof forceCheck != 'undefined';
+    //se um parent foi clickado, o check é forçado
+    if (isForced) node.check = forceCheck;
+      //senao, faz o inverso do check atual
+    else node.check = !node.check;
+    //pega o check button do nó atual
+    var $node = $("#node-" + node.id + " > .checkButton");
+    $node.prop("checked", node.check);
+    //se houver filhos, varre os filhos, chama a mesma função recursivamente
+    if (node.childs.length) {
+        node.childs.forEach(function(i){
+          clickCheck.call($node, FileTreeOptions.data["node-" + i], node.check);
+        });
+    }
+    //se houver um parent
+    if (!isForced && node.idParent) {
+      let $parent = $("#node-" + node.idParent + " > .checkButton");
+      if(node.check) checkParent.call($parent, FileTreeOptions.data["node-" + node.idParent], node.check);
+    }
+    //isso vai ser tenso..
+    FileTreeOptions.arrSelected.push(node.id);
   }
+
+  function checkParent(nodeParent, isChecked) {
+    console.log(this);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })(jQuery);
